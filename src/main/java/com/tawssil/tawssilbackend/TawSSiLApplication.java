ab4c4e20bdde3ca480e85e814755d6d803ff9245
abdellah <abdellahloulanti234@gmail.com>
2024-10-20 16:13:02 +0200

    Initial commit for TawSSiL

---
+++ src/main/java/com/tawssil/tawssilbackend/TawSSiLApplication.java
@@ -0,0 +1,13 @@
+package com.tawssil.tawssilbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawSSiLApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TawSSiLApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tawssil/tawssilbackend/TawSSiLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawssil.tawssilbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawSSiLApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
